bc83965260cbd6c691b2a3034403cf18a8a7a61a
Andrey Kuznetsov <daniilbacardi@yandex.ru>
2026-08-03 21:25:33 +0300

    feat(runtime): expose reusable runtime composition

---
+++ qaip-core/src/main/java/ru/kuznetsov/qaip/runtime/QaipRuntimeFactory.java
@@ -0,0 +1,60 @@
+package ru.kuznetsov.qaip.runtime;
+
+import ru.kuznetsov.qaip.core.application.importing.DefaultProjectImporter;
+import ru.kuznetsov.qaip.core.application.importing.ProjectImporter;
+import ru.kuznetsov.qaip.core.application.importproject.DefaultImportProjectUseCase;
+import ru.kuznetsov.qaip.core.application.persistence.DefaultPersistProject;
+import ru.kuznetsov.qaip.core.application.query.node.ProjectNodeLookup;
+import ru.kuznetsov.qaip.core.application.query.nodedetails.DefaultNodeDetailsUseCase;
+import ru.kuznetsov.qaip.core.application.query.nodedetails.NodeDetailsMapper;
+import ru.kuznetsov.qaip.core.application.query.projectsummary.DefaultProjectSummaryUseCase;
+import ru.kuznetsov.qaip.core.application.query.projectsummary.ProjectSummaryMapper;
+import ru.kuznetsov.qaip.core.application.query.relationship.DefaultRelationshipsUseCase;
+import ru.kuznetsov.qaip.core.application.query.relationship.ProjectRelationshipLookup;
+import ru.kuznetsov.qaip.core.application.query.relationship.RelationshipDetailsMapper;
+import ru.kuznetsov.qaip.core.application.query.trace.DefaultTraceUseCase;
+import ru.kuznetsov.qaip.core.application.query.trace.TraceGraphBuilder;
+import ru.kuznetsov.qaip.core.application.query.trace.TraceMapper;
+import ru.kuznetsov.qaip.core.application.query.validation.DefaultValidationUseCase;
+import ru.kuznetsov.qaip.core.application.query.validation.ValidationReportMapper;
+import ru.kuznetsov.qaip.core.application.validation.ValidationEngine;
+import ru.kuznetsov.qaip.core.application.validation.rule.IsolatedNodeValidationRule;
+import ru.kuznetsov.qaip.core.application.validation.rule.ScenarioWithoutTestValidationRule;
+import ru.kuznetsov.qaip.core.importing.binding.DefaultProjectBinder;
+import ru.kuznetsov.qaip.core.importing.parsing.JacksonProjectJsonParser;
+import ru.kuznetsov.qaip.core.importing.parsing.NetworkntProjectSchemaValidator;
+import ru.kuznetsov.qaip.core.persistence.memory.InMemoryProjectReader;
+import ru.kuznetsov.qaip.core.persistence.memory.InMemoryProjectRepository;
+import ru.kuznetsov.qaip.core.validation.DefaultProjectApplicationValidator;
+
+import java.util.List;
+
+public final class QaipRuntimeFactory {
+    private QaipRuntimeFactory() { }
+
+    public static QaipRuntime inMemory() {
+        InMemoryProjectRepository repository = new InMemoryProjectRepository();
+        InMemoryProjectReader reader = new InMemoryProjectReader(repository);
+        ProjectImporter importer = new DefaultProjectImporter(
+                new JacksonProjectJsonParser(),
+                new NetworkntProjectSchemaValidator(),
+                new DefaultProjectBinder(),
+                new DefaultProjectApplicationValidator());
+        var importUseCase = new DefaultImportProjectUseCase(
+                importer, new DefaultPersistProject(repository));
+
+        ProjectNodeLookup nodeLookup = new ProjectNodeLookup();
+        ValidationEngine validationEngine = new ValidationEngine(List.of(
+                new IsolatedNodeValidationRule(), new ScenarioWithoutTestValidationRule()));
+
+        return new QaipRuntime(
+                importUseCase,
+                reader,
+                new DefaultProjectSummaryUseCase(reader, new ProjectSummaryMapper()),
+                new DefaultNodeDetailsUseCase(reader, nodeLookup, new NodeDetailsMapper()),
+                new DefaultRelationshipsUseCase(
+                        reader, nodeLookup, new ProjectRelationshipLookup(), new RelationshipDetailsMapper()),
+                new DefaultTraceUseCase(reader, nodeLookup, new TraceGraphBuilder(), new TraceMapper()),
+                new DefaultValidationUseCase(reader, validationEngine, new ValidationReportMapper()));
+    }
+}
